1f030de85e82196a0e1bfa5fd38d21656ba8eb7b
Stlightz <mishakusq@gmail.com>
2026-08-18 20:38:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 export default{
   async fetch(request, env){
+    if(request.method !== "POST"{
     return new Response("Cb s alive");
+  }
+    const update = await request.json();
+    console.log(update);
+    return new Responce ("OK");
   }
 };
